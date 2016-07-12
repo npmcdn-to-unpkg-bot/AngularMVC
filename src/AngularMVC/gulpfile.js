a9d4946e51194a0ce99b49cb6c2b80fe10b06ed0
Martin <Martin@MARTIN-PC>
2016-07-12 21:38:21 +0200

    Add project files.

---
+++ src/AngularMVC/gulpfile.js
@@ -0,0 +1,56 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var clean = require('gulp-clean');
+
+var destPath = './wwwroot/libs/';
+
+// Delete the dist directory
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+gulp.task("scriptsAndStyles", () => {
+    gulp.src([
+            'core-js/client/**',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/**',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'jquery/dist/jquery.*js',
+            'bootstrap/dist/js/bootstrap.*js',
+    ], {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest("./wwwroot/libs"));
+});
+
+var tsProject = ts.createProject('scripts/tsconfig.json');
+gulp.task('ts', function (done) {
+    //var tsResult = tsProject.src()
+    var tsResult = gulp.src([
+            "scripts/*.ts"
+    ])
+        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+    return tsResult.js.pipe(gulp.dest('./wwwroot/appScripts'));
+});
+
+gulp.task('watch', ['watch.ts']);
+
+gulp.task('watch.ts', ['ts'], function () {
+    return gulp.watch('scripts/*.ts', ['ts']);
+});
+
+gulp.task('default', ['scriptsAndStyles', 'watch']);
+
+
+
+//gulp.task('default', function () {
+//    // place code for your default task here
+//});
---
+++ src/AngularMVC/wwwroot/appScripts/boot.js
@@ -0,0 +1,6 @@
+"use strict";
+//<reference path="./../typings/globals/core-js/index.d.ts"/>
+const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+const app_1 = require('./app');
+platform_browser_dynamic_1.bootstrap(app_1.AppComponent);
+//# sourceMappingURL=boot.js.map
